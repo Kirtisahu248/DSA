753985425720c6b8d63a5c778eba5fc5f93a587b
Kirti Sahu <138589688+Kirtisahu248@users.noreply.github.com>
2024-06-24 23:42:18 +0530

    Update IndexesofSubarraySum.java

--- Day-24/IndexesofSubarraySum.java
+++ Day-24/IndexesofSubarraySum.java
@@ -31,4 +31,34 @@ class IndexesofSubarraySum{
     }
     return ans;
     }
+    public static void main(String[] args) {
+        // Initialize Scanner to take input
+        Scanner sc = new Scanner(System.in);
+
+        // Taking array size and sum input
+        System.out.println("Enter the size of the array:");
+        int n = sc.nextInt();
+        System.out.println("Enter the sum to find:");
+        int s = sc.nextInt();
+
+        // Taking array elements input
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Call the subarraySum function
+        ArrayList<Integer> result = IndexesofSubarraySum.subarraySum(arr, n, s);
+
+        // Print the result
+        if (result.size() == 1 && result.get(0) == -1) {
+            System.out.println("No subarray with given sum found");
+        } else {
+            System.out.println("Subarray with given sum is found between indexes: " + result.get(0) + " and " + result.get(1));
+        }
+
+        // Close the scanner
+        sc.close();
+    }
 }
